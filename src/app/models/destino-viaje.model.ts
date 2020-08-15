4676b23dfaa49dd346b13a39c2ac225210d6da44
Jamyth Ariza <jamythariza@gmail.com>
2020-08-15 11:32:18 -0500

    Evaluación Semana 1

---
+++ src/app/models/destino-viaje.model.ts
@@ -0,0 +1,11 @@
+export class DestinoViaje {
+
+    nombre: string;
+    url: string;
+
+    constructor(nombres:string,urls:string){
+        this.nombre= nombres;
+        this.url=urls;
+
+    }
+}
